335b286955ae512039a5aff4aac47708b521186a
Nugget-Meister <tremainecamacho@gmail.com>
2024-12-03 02:19:18 -0500

    init queries

---
+++ queries/users.js
@@ -0,0 +1,32 @@
+let db = require('../db/dbConfig.js')
+
+let getUser = () => {
+    try {
+        
+    } catch (error) {
+        return error
+    }
+}
+let getAllUsers = () => {
+    try {
+        
+    } catch (error) {
+        return error
+    }
+}
+let createUser = () => {
+    try {
+        
+    } catch (error) {
+        return error
+    }
+}
+let updateUser = () => {}
+let deleteUser = () => {}
+
+module.exports = {
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser
+}
