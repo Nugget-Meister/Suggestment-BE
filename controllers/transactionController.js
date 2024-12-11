b9cab8ff959f5ecdf88f3fbd79906b3d3571548c
Nugget-Meister <tremainecamacho@gmail.com>
2024-12-11 03:25:17 -0500

    fix token on transactions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,21 +1,18 @@
 const express = require('express')
-const { getTransactions } = require('../queries/transactions')
-
+const { getTransactions,createTransaction, getTransaction,updateTransaction,deleteTransaction } = require('../queries/transactions')
+const jwt = require('jsonwebtoken')
 const transactions = express.Router()
 
 transactions.get('/user/:id', async (req,res) => {
     let { id } = req.params
-    // console.log(req.params)
     const result = await getTransactions(id)
     process.stdout.write(`Transactions Request for: ${id} `)
     
-
     if(result){
         if(result.severity != undefined){
             console.log("Error Detected: ", result.message)
             res.status(500).json({
                 message:"BAD",
-
                 data: result
             })
         } else {
@@ -27,6 +24,181 @@ transactions.get('/user/:id', async (req,res) => {
         }
     }
 })
+
+transactions.get('/:id', (req, res)=> {
+    const { id } = req.params;
+    let sessionToken = req.headers.authorization.split(' ')[1]
+    console.log("Transaction request for: ", id)
+    console.log(sessionToken)
+
+    try {
+        jwt.verify(sessionToken, 'sessionToken', async (error, decoded) => {
+            if(error){
+                console.log("Invalid Token Detected")
+                res.status(500).json({
+                    message: "BAD",
+                    details:"Bad token",
+                    data:null
+                })
+            } else {
+                // Do things here
+                let result = await getTransaction(id)
+                if(!result.severity){
+                    res.status(200).json({
+                        message:"OK",
+                        details:"transaction",
+                        data: result,
+                    })
+                } else {
+                    res.status(404).json({
+                        message:"BAD",
+                        details:"unable to find transaction",
+                        data: null,
+                    })
+                }
+            }
+        })
+    } catch (error) {
+        console.log("Invalid Token Detected")
+        res.status(500).json({
+            message: "BAD",
+            details:"Bad token",
+            data:null
+        })
+    }
+})
+
+transactions.post('/', (req, res)=> {
+    const sessionToken = req.headers.authorization.split(' ')[1]
+    const data = req.body
+
+    try {
+        jwt.verify(sessionToken, 'sessionToken', async (error, decoded) => {
+            if(error){
+                console.log("Invalid Token Detected")
+                res.status(500).json({
+                    message: "BAD",
+                    details:"Bad token",
+                    data:null
+                })
+            } else {
+                // Do things here
+                let result = await createTransaction(data)
+                if(!result.severity){
+                    res.status(200).json({
+                        message:"OK",
+                        details:"transaction created successfully",
+                        data: result,
+                    })
+                } else {
+                    res.status(404).json({
+                        message:"BAD",
+                        details:"unable to create transaction",
+                        data: null,
+                    })
+                }
+            }
+
+        })
+    } catch (error) {
+        console.log("Invalid Token Detected")
+        res.status(500).json({
+            message: "BAD",
+            details:"Bad token",
+            data:null
+        })
+    }
+})
+
+transactions.put('/:id', (req, res)=> {
+    const { id } = req.params;
+    const sessionToken = req.headers.authorization.split(' ')[1]
+
+    const data = req.body
+    
+    try {
+        jwt.verify(sessionToken, 'sessionToken', async (error, decoded) => {
+            if(error){
+                console.log("Invalid Token Detected")
+                res.status(500).json({
+                    message: "BAD",
+                    details:"Bad token",
+                    data:null
+                })
+            } else {
+                // Do things here
+                let result = await updateTransaction(data)
+                if(!result.severity){
+                    res.status(200).json({
+                        message:"OK",
+                        details:"transaction updated",
+                        data: result,
+                    })
+                } else {
+                    res.status(404).json({
+                        message:"BAD",
+                        details:"unable to find transaction",
+                        data: null,
+                    })
+                }
+
+            }
+
+        })
+    } catch (error) {
+        console.log("Invalid Token Detected")
+        res.status(500).json({
+            message: "BAD",
+            details:"Bad token",
+            data:null
+        })
+    }
+})
+
+transactions.delete('/:id', (req, res)=> {
+    const { id } = req.params;
+    const sessionToken = req.headers.authorization.split(' ')[1]
+    try {
+        jwt.verify(sessionToken, 'sessionToken', async (error, decoded) => {
+            if(error){
+                console.log("Invalid Token Detected")
+                res.status(500).json({
+                    message: "BAD",
+                    details:"Bad token",
+                    data:null
+                })
+            } else {
+                // Do things here
+                const result = await deleteTransaction(id)
+                if(!result.severity){
+                    res.status(200).json({
+                        message:"OK",
+                        details:"transaction",
+                        data: result,
+                    })
+                } else {
+                    res.status(404).json({
+                        message:"BAD",
+                        details:"unable to find transaction",
+                        data: null,
+                    })
+                }
+
+
+            }
+
+        })
+    } catch (error) {
+        console.log("Invalid Token Detected")
+        res.status(500).json({
+            message: "BAD",
+            details:"Bad token",
+            data:null
+        })
+    }
+})
+
+
 
 transactions.get('*', (req, res)=> {
     res.send('Base path')
--- queries/transactions.js
+++ queries/transactions.js
@@ -9,6 +9,51 @@ const getTransactions = async (id) => {
     }
 }
 
+const getTransaction = async (id) => {
+    try {
+        const result = await db.one("SELECT * FROM suggestment_transactions WHERE transaction_id=$1", [id])
+        return result
+    } catch (error) {
+        return error
+    }
+}
+const createTransaction = async (data) => {
+    try {
+        const result = await db.one(
+            "INSERT INTO suggestment_transactions (user_id, source_user_id, details, category, amount, date) VALUES ($1, null, $2, $3, $4, $5) RETURNING *", 
+            [data.user_id, data.details, data.category, data.amount, data.date]
+        )
+        return result
+    } catch (error) {
+            return error
+    }
+}
+const updateTransaction = async (data) => {
+    try {
+        const result = await db.one(
+            "UPDATE suggestment_transactions SET (user_id, source_user_id, details, category, amount, date) VALUES ($1, null, $2, $3, $4, $5) WHERE transaction_id=$6 RETURNING *", 
+            [data.user_id, data.details, data.category, data.amount, data.date, data.transaction_id]
+        )
+        return result
+    } catch (error) {
+        return error
+    }
+}
+const deleteTransaction = async (data) => {
+    try {
+        const result = await db.one(
+            "DELETE FROM suggestment_transactions WHERE transaction_id=$1 RETURNING *", 
+            [data.transaction_id]
+        )
+        return result
+    } catch (error) {
+        return error
+    }
+}
+
+
+
+
 const getAllTransactions = async () => {
     try {
         const result = await db.any("SELECT * FROM suggestment_transactions",)
@@ -20,5 +65,9 @@ const getAllTransactions = async () => {
 
 module.exports = {
     getTransactions, 
-    getAllTransactions
+    getAllTransactions,
+    createTransaction,
+    getTransaction,
+    updateTransaction,
+    deleteTransaction
 }
